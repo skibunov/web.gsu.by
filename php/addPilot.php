e3b2d6724f4ae451185e9aa733399b9ea5653369
skibunov <andrey.skibunov2001@gmail.com>
2021-02-19 13:36:34 +0300

    lab_1

--- php/addPilot.php
+++ php/addPilot.php
@@ -1,25 +1,29 @@
 <?php 
 
-	if(isset($_POST['namePilot']))
-		$namePilot = $_POST['namePilot'];
+	if(isset($_GET['namePilot']))
+		$namePilot = $_GET['namePilot'];
 
-	if(isset($_POST['surnamePilot']))
-		$surnamePilot = $_POST['surnamePilot'];
+	if(isset($_GET['surnamePilot']))
+		$surnamePilot = $_GET['surnamePilot'];
 
-	if(isset($_POST['middleNamePilot']))
-		$middleNamePilot = $_POST['middleNamePilot'];
+	if(isset($_GET['middleNamePilot']) && $_GET['middleNamePilot'] != '')
+		$middleNamePilot = $_GET['middleNamePilot'];
+	else
+		$middleNamePilot = "Не указано";
 
-	if(isset($_POST['positionPilot']))
-		$positionPilot = $_POST['positionPilot'];
+	if(isset($_GET['positionPilot']))
+		$positionPilot = $_GET['positionPilot'];
 
-	if(isset($_POST['birthdayPilot']))
-		$birthdayPilot = $_POST['birthdayPilot'];
+	if(isset($_GET['birthdayPilot']))
+		$birthdayPilot = $_GET['birthdayPilot'];
 
-	if(isset($_POST['adressPilot']))
-		$adressPilot = $_POST['adressPilot'];
+	if(isset($_GET['adressPilot']) && $_GET['adressPilot'] != '')
+		$adressPilot = $_GET['adressPilot'];
+	else
+		$adressPilot = "Не указано";
 
-	if(isset($_POST['telPilot']))
-		$telPilot = $_POST['telPilot'];
+	if(isset($_GET['telPilot']))
+		$telPilot = $_GET['telPilot'];
 
 
 	// echo json_encode(array(
@@ -35,28 +39,16 @@
 
  ?>
 
-
-<!doctype html>
-<html lang="ru">
-<head>
-  <!-- Required meta tags -->
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-
-  <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="../vendor/bootstrap/dist/css/bootstrap.min.css">
-
-  <title>Hello, world!</title>
-</head>
-<body>
-	
+<?php if (isset($_GET['submit'])) { ?>
+	<div class="col-10 mx-auto">
+	<h1 class="text-center">Таблица Пилотов</h1>
 	<table class="table table-bordered">
 	  <thead>
 	    <tr>
 	      <th scope="col">Имя</th>
-	      <th scope="col">Фамилия </th>
-	      <th scope="col">Отчество </th>
-	      <th scope="col">Должность </th>
+	      <th scope="col">Фамилия</th>
+	      <th scope="col">Отчество</th>
+	      <th scope="col">Должность</th>
 	      <th scope="col">Дату рождения</th>
 	      <th scope="col">Адрес</th>
 	      <th scope="col">Мобильный телефон</th>
@@ -64,7 +56,7 @@
 	  </thead>
 	  <tbody>
 	    <tr>
-	      <th><?php echo $namePilot; ?></th>
+	      <td><?php echo $namePilot; ?></ts>
 	      <td><?php echo $surnamePilot; ?></td>
 	      <td><?php echo $middleNamePilot; ?></td>
 	      <td><?php echo $positionPilot; ?></td>
@@ -74,7 +66,5 @@
 	    </tr>
 	  </tbody>
 	</table>
-
-
-</body>
-</html>
+		</div>
+<?php } ?>
